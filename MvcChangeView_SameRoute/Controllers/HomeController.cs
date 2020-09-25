afd25d65edf61f13b52ca5ec38b9cc85b88b5883
Deybson Ferreira <deybson.ferreira@iatec.com>
2020-09-25 12:46:49 -0300

    add project

---
+++ MvcChangeView_SameRoute/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+
+namespace MvcChangeView_SameRoute.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Country(int id)
+        {
+            if (id == 1)
+                return View("Brasil");
+
+            if (id == 2)
+                return View("Chile");
+
+            if (id == 3)
+                return View("Paraguai");
+
+            if (id == 4)
+                return View("Uruguai");
+
+            return View();
+        }
+
+    }
+}
